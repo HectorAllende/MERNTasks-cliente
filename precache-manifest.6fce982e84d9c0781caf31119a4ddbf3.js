c4f58600e390c0a8627c8b76b93015a6724c27df
HecAllende <54869462+HectorAllende@users.noreply.github.com>
2020-11-16 00:48:33 -0300

    deploimnet

---
+++ precache-manifest.6fce982e84d9c0781caf31119a4ddbf3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71ad6fd9dd70dde52f7f1f971a528af8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "02d9ae87cb7185a16663",
+    "url": "/static/css/main.d862db12.chunk.css"
+  },
+  {
+    "revision": "1edc989ba567260c9abc",
+    "url": "/static/js/2.ec6cb7c4.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.ec6cb7c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02d9ae87cb7185a16663",
+    "url": "/static/js/main.f9bf698c.chunk.js"
+  },
+  {
+    "revision": "b392ba862a5bbfe037c8",
+    "url": "/static/js/runtime-main.f24b64df.js"
+  }
+]);
